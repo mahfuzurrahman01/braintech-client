5ec1262df988792e7bf81fc07361d2560b36a98c
mahfuzurrahman01 <mahfuzurrahman01010101@gmail.com>
2022-10-27 17:12:13 +0600

    loading animation added

--- src/PrivateRoute/Private.jsx
+++ src/PrivateRoute/Private.jsx
@@ -3,14 +3,18 @@ import { useContext } from 'react';
 import { Navigate, useLocation } from 'react-router-dom';
 import { AuthContext } from '../AuthContext/UserContext';
 
-const Private = ({children}) => {
-    const { user,loading } = useContext(AuthContext)
+const Private = ({ children }) => {
+    const { user, loading } = useContext(AuthContext)
     const location = useLocation()
-    if(loading){
-        return <h1>loading...</h1>
+    if (loading) {
+        return <div className="flex items-center justify-center mt-10 space-x-2">
+            <div className="w-4 h-4 rounded-full animate-pulse bg-gray-500"></div>
+            <div className="w-4 h-4 rounded-full animate-pulse bg-gray-500"></div>
+            <div className="w-4 h-4 rounded-full animate-pulse bg-gray-500"></div>
+        </div>
     }
-    if(!user){
-        return <Navigate to='/login' state={{from:location}} replace></Navigate>
+    if (!user) {
+        return <Navigate to='/login' state={{ from: location }} replace></Navigate>
     }
     return children
 };
