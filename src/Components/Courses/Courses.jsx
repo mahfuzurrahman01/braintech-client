2a9dff2f30683954624b8b41216f3dae82ea2da7
mahfuzurrahman01 <mahfuzurrahman01010101@gmail.com>
2022-10-27 22:17:36 +0600

    responsive issue solve

--- src/Components/Courses/Courses.jsx
+++ src/Components/Courses/Courses.jsx
@@ -8,11 +8,11 @@ const Courses = () => {
     const courses = useLoaderData()
     return (
         <div>
-            <div className='md:w-11/12  w-full mx-auto flex mt-10 gap-x-5 px-5'>
-                <div className='md:w-1/5 w-1/4'>
+            <div className='md:w-11/12  w-full mx-auto flex md:flex-row flex-col md:mt-10 mt-5 md:gap-x-5 gap-y-5 px-5'>
+                <div className='md:w-1/5 w-full'>
                     <LeftNav></LeftNav>
                 </div>
-                <div className='md:w-4/5 w-3/4 grid lg:grid-cols-3 md:grid-cols-2 grid-cols-1 gap-5'>
+                <div className='md:w-4/5 w-full mx-auto grid lg:grid-cols-3 md:grid-cols-2 grid-cols-1 gap-5'>
                     {
                         courses.map(course => <Card key={course.id} course={course}></Card>)
                     }
